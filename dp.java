f94a1a79282085e2fb01b2b16bb7a8e7d43d832c
Vignesh Reddy <kakarlavigneshreddy@gmail.com>
2026-03-24 22:48:32 +0530

    added oen more code on dp

--- dp.java
+++ dp.java
@@ -16,3 +16,32 @@ class Solution {
         return sell2;
     }
 }
+
+// best time to buy and sell stock IV
+
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int n=prices.length;
+        int profit=0;
+        if(k>=n/2){
+            for(int i=1;i<n;i++){
+                if(prices[i]>prices[i-1]){
+                    profit+=prices[i]-prices[i-1];
+                }
+            }
+            return profit;
+        }
+        int[] buy=new int[k+1];
+        int[] sell=new int[k+1];
+        for(int i=0;i<=k;i++){
+            buy[i]=Integer.MIN_VALUE;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=k;j++){
+                buy[j]=Math.max(buy[j],sell[j-1]-prices[i]);
+                sell[j]=Math.max(sell[j],buy[j]+prices[i]);
+            }
+        }
+        return sell[k];
+    }
+}
